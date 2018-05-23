f6fd9cb13986eb8d88b3e83dcee2bdc65e76c066
Walter Sanchez <walteranyika@gmail.com>
2018-05-23 14:26:18 +0300

    Fused Location API Example

---
+++ app/src/main/java/com/walter/fusedlocationprovider/MapsActivity.java
@@ -0,0 +1,264 @@
+package com.walter.fusedlocationprovider;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Build;
+import android.os.Looper;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Dash;
+import com.google.android.gms.maps.model.Gap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PatternItem;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.SphericalUtil;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
+    SupportMapFragment mapFragment;
+
+    LocationRequest mLocationRequest;
+    Location mLastLocation;
+    Marker mCurrLocationMarker, mDestinationMarker;
+    FusedLocationProviderClient mFusedLocationClient;
+    boolean destinationSelected=false;
+
+    //Add this line to gradle
+    //implementation 'com.google.maps.android:android-maps-utils:0.5'
+
+    //Download two markers , one for origin and another for destination from this url
+    //https://www.iconfinder.com/icons/73012/azure_ball_base_lumina_map_marker_icon#size=32
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        //Try different maps like HYBRID
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000); // two minute interval
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+                //Location Permission already granted
+                mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+                mMap.setMyLocationEnabled(true);
+            } else {
+                //Request Location Permission
+                checkLocationPermission();
+            }
+        }
+        else {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+            //mMap.setMyLocationEnabled(true);
+        }
+
+        //Listener when you click on the map. We will use this to set the destination
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng destination) {
+                mMap.clear();
+                mCurrLocationMarker= mMap.addMarker(new MarkerOptions().position(new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude()))
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker))
+                        .title("You"));
+
+                mDestinationMarker= mMap.addMarker(new MarkerOptions().position(destination)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.dest_marker))
+                        .title("Destination"));
+
+                destinationSelected=true;
+
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+                builder.include(mCurrLocationMarker.getPosition());
+                builder.include(mDestinationMarker.getPosition());
+
+                LatLngBounds bounds = builder.build();
+                int padding = 250; // offset from edges of the map in pixels
+                CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+                mMap.moveCamera(cu);
+                mMap.animateCamera(cu);
+
+                LatLng startPoint = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+                showCurvedPolyline(startPoint, destination,0.2);
+            }
+        });
+    }
+
+    /**
+     * Core of the app. The location callback handles location updates. If The GPS location changes, that GPS data is brought to This function
+     */
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            List<Location> locationList = locationResult.getLocations();
+            if (locationList.size() > 0) {
+                //The last location in the list is the newest
+                Location location = locationList.get(locationList.size() - 1);
+                Log.i("MapsActivity", "Location: " + location.getLatitude() + " " + location.getLongitude());
+                mLastLocation = location;
+                if (mCurrLocationMarker != null) {
+                    mCurrLocationMarker.remove();
+                }
+                //Place current location marker
+                //Download this marker, rename and add it your drawable folder
+                //https://www.iconfinder.com/icons/73017/ball_base_map_marker_pink_icon#size=32
+                mCurrLocationMarker=mMap.addMarker(new MarkerOptions().position(new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude()))
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker))
+                        .title("You"));
+                if (destinationSelected==false)
+                   mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude()),17.0f));
+            }
+        }
+    };
+
+
+
+    /**
+     * method to ask user for permission to access GPS on the phone
+     */
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,  Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Location Permission Needed")
+                        .setMessage("This app needs the Location permission, please accept to use location functionality")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSIONS_REQUEST_LOCATION );
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION );
+            }
+        }
+    }
+
+    /**
+     * Method to handle the results if user granted the app permission or not
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,  Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+                        mMap.setMyLocationEnabled(true);
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+
+    /**
+     * Draw a curved polyline on the map like uber
+     */
+    private void showCurvedPolyline (LatLng p1, LatLng p2, double k) {
+        //Calculate distance and heading between two points
+        double d = SphericalUtil.computeDistanceBetween(p1,p2);
+        double h = SphericalUtil.computeHeading(p1, p2);
+
+        //Midpoint position
+        LatLng p = SphericalUtil.computeOffset(p1, d*0.5, h);
+
+        //Apply some mathematics to calculate position of the circle center
+        double x = (1-k*k)*d*0.5/(2*k);
+        double r = (1+k*k)*d*0.5/(2*k);
+
+        LatLng c = SphericalUtil.computeOffset(p, x, h + 90.0);
+
+        //Polyline options
+        PolylineOptions options = new PolylineOptions();
+        List<PatternItem> pattern = Arrays.<PatternItem>asList(new Dash(30), new Gap(20));
+
+        //Calculate heading between circle center and two points
+        double h1 = SphericalUtil.computeHeading(c, p1);
+        double h2 = SphericalUtil.computeHeading(c, p2);
+
+        //Calculate positions of points on circle border and add them to polyline options
+        int numpoints = 100;
+        double step = (h2 -h1) / numpoints;
+
+        for (int i=0; i < numpoints; i++) {
+            LatLng pi = SphericalUtil.computeOffset(c, r, h1 + i * step);
+            options.add(pi);
+        }
+        //Draw polyline
+        mMap.addPolyline(options.width(8).color(Color.BLACK).geodesic(false).pattern(pattern));
+    }
+
+    //END OF CODE
+
+
+}
